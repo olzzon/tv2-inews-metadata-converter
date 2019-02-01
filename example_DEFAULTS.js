63edf1304dad0954e5b17da25a4ca8c9c5a3c72e
olzzon <github@olzzon.dk>
2019-02-01 11:12:45 +0100

    Updated example_DEFAULT.js

--- example_DEFAULTS.js
+++ example_DEFAULTS.js
@@ -1,3 +1,9 @@
 export const SERVERS = ["xx.xx.xx.xx","yy.yy.yy.yy"];
 export const USERNAME = "iNewsUserName";
 export const PASSWORD = "iNewsPassword";
+export const INEWS_QUEUE = "INEWS.QUEUE.ON-AIR";
+
+export const OVERLAY_TEMPLATES = {
+    "bund": "/HTML-Bundt/BUNDT",
+    "topt": "/HTML-Topt/TOPT"
+};
